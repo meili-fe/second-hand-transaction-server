38240aa53b1b203f4c668462401af7f7308678c1
梁景先 <jingxian.liang@mljr.com>
2019-10-23 18:48:21 +0800

    update

---
+++ controller/product.js
@@ -0,0 +1,74 @@
+const { query } = require("../db");
+
+// 查询所有产品
+let findProduct = function(params) {
+  let { title, pageSize, page } = params;
+  let offset = (page - 1) * pageSize;
+  let sql = `SELECT 
+    p.id,p.title,p.location,p.price,p.contact,p.description,p.status,
+    c.name category_name,
+    GROUP_CONCAT( p_img.img_url ) AS img_list     
+    FROM product p
+    LEFT JOIN product_img p_img ON p.id = p_img.pro_id 
+    LEFT JOIN category c ON p.cate_id = c.id 
+    `;
+
+  if (title) {
+    offset = 0;
+    sql += ` WHERE p.title like ? `;
+  }
+  sql += ` GROUP BY p.id ORDER BY p.create_time DESC  limit ${offset},${pageSize}  `;
+
+  let value = ["%" + title + "%"];
+  return query(sql, value);
+};
+// 查询产品总数
+let findProductCount = function(params) {
+  let { id } = params;
+  let sql = `SELECT count(*) FROM product `;
+  if (id) {
+    sql += ` WHERE id = ?  `;
+  }
+  let value = [id];
+  return query(sql, value);
+};
+// 添加商品
+let insertProduct = function(params) {
+  let { cate_id, title, location, price, description, contact } = params;
+  let sql =
+    "INSERT INTO product (cate_id,title,location,price,description,contact,status) VALUES (?,?,?,?,?,?,?)";
+  let value = [cate_id, title, location, price, description, contact, 0];
+  return query(sql, value);
+};
+// 修改商品信息
+let updateProduct = function(params) {
+  let { title, location, price, description, contact } = params;
+  let sql =
+      "UPDATE product SET title=?,location=?,price=?,description=?,contact=? WHERE id=?",
+    value = [title, location, price, description, contact];
+  return query(sql, value);
+};
+// 修改商品信息状态(0 发布 1 已卖出 2 关闭)
+let updateProductSataus = function(params) {
+  let { status } = params;
+  let sql = "UPDATE product SET status=? WHERE id=?",
+    value = [status];
+  return query(sql, value);
+};
+// 删除商品信息
+let deleteProductById = function(params) {
+  let { id } = params;
+  let sql =
+    "DELETE p,p_img FROM product p LEFT JOIN product_img p_img  ON p.id = p_img.pro_id WHERE id = ?";
+  let value = [id];
+  return query(sql, value);
+};
+
+module.exports = {
+  findProduct,
+  findProductCount,
+  insertProduct,
+  updateProduct,
+  updateProductSataus,
+  deleteProductById,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const user = require("./user");
+const product = require("./product");
 const upload = require("./upload");
 module.exports = function(app) {
   app.use(user.routes()).use(user.allowedMethods());
+  app.use(product.routes()).use(product.allowedMethods());
   app.use(upload.routes()).use(upload.allowedMethods());
 };

3d98193e7cf02e31313df3ddcf4fd9669740f0fd
梁景先 <jingxian.liang@mljr.com>
2019-10-28 14:57:24 +0800

    update

--- db/config.js
+++ db/config.js
@@ -2,14 +2,14 @@
 const config = {
   port: 3306,
   database: {
-    DATABASE: "femans_mljr", //数据库
-    USERNAME: "femans_mljr", //用户
-    PASSWORD: "password", //密码
-    PORT: "3306", //端口
-    HOST: "femans.com", //服务ip地址
+    DATABASE: 'femans_mljr', //数据库
+    USERNAME: 'femans_mljr', //用户
+    PASSWORD: 'password', //密码
+    PORT: '3306', //端口
+    HOST: 'femans.com', //服务ip地址
   },
-  appURL:"www.femans.com",
-  appId: "wxf07478d878a186e2",
-  secret: "3cafd782558a1439b5b2f2799dd04374",
+  appURL: 'second-hand.femans.com', //项目根域名
+  appId: 'wxf07478d878a186e2',
+  secret: '3cafd782558a1439b5b2f2799dd04374',
 };
 module.exports = config;
